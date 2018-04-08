a757d5d7be5b849210bd99fe9530676d2bb1f2af
Saiko <a.m.abdelmottaleb@gmail.com>
2018-04-08 15:59:07 +0200

    -fixed a bug where pressing back instead of signing in causes to bring the user back to the sign in layout

--- app/src/main/java/com/saiko/firechat/MainActivity.java
+++ app/src/main/java/com/saiko/firechat/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.saiko.firechat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -163,6 +164,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Signed out!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        }
     }
 
     @Override
